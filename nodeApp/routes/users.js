07a8598b30bfdae86a60365e16383cfd7a252d21
pingkoc <pingkoc@outlook.com>
2015-10-23 00:35:16 -0500

    updated node without databse

--- nodeApp/routes/users.js
+++ nodeApp/routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var spawn = require('child_process').spawn;
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -9,13 +11,39 @@ router.get('/', function(req, res, next) {
 /* Receive POST analyze URL */
 router.post('/analyzeURL', function(req, res, next) {
     console.log('Received: ' + req.body.url);
-    var data = {title: 'Booya',
-                description: 'blahBlah',
-                senRate: '0.67',
-                techRate: '0.25',
-                url: req.body.url,
-                read: 'Unread'};
-    res.json(data);
+    var py = spawn('python', ['./scripts/test.py']);
+    py.stdout.on('data', function(data) {
+        console.log(JSON.parse(data.toString()));
+        res.json(JSON.parse(data.toString()));
+    });
+});
+
+/* Receive POST crawl BBC World */
+router.post('/crawlBBCWorld', function(req, res, next) {
+    console.log('Received: BBC Crawl');
+    var dummy = [{'title': 'beeboo', 'description': 'blublu', 'senRate': '-', 'readRate': '0.2', 'url': 'blayyy', 'read': 'Unread'}, {'title': 'babbu', 'description': 'a15462', 'senRate': '-', 'readRate': '0.6', 'url': 'asdfasdfa', 'read': 'Unread'},{'title': 'bibibibib', 'description': 'lalallaa', 'senRate': '-', 'readRate': '0.9', 'url': 'huhuhuhuh', 'read': 'Unread'}]; 
+    res.json(dummy);
+});
+
+/* Receive POST delete */
+router.post('/remove', function(req, res, next) {
+    console.log('Received: Remove Request ' + req.body.url);
+    //DO REMOVE HERE
+    res.send('Removed');
+});
+
+/* Receive POST update Read */
+router.post('/updateRead', function(req, res, next) {
+    console.log('Received: Update Read Request ' + req.body.url);
+    //DO UPDATE HERE
+    res.send('Read Updated');
+});
+
+/* Receive POST Query */
+router.post('/readQuery', function(req, res, next) {
+    console.log('Received: Readability Query Request ' + req.body.type);
+    //DO UPDATE HERE
+    res.send([]);
 });
 
 module.exports = router;
