b1ecfa38aaa718f0bbe9c976a033e6f83d118e5d
suree404 <sjiang@wirelessprvnat-172-17-198-244.near.illinois.edu>
2015-10-23 00:33:23 -0500

    insert delete ...

---
+++ crawler/SqlAdd.py
@@ -0,0 +1,86 @@
+def single_add(url):
+    import mysql.connector
+    import json
+    from merge import one_website_return
+
+    cnx = mysql.connector.connect(user='root', password='1111',
+                                  database='start')
+
+    cursor = cnx.cursor()
+
+    add_demo = ("INSERT INTO demo "
+                "(title, description, senRate, readRate, url, category, readby) "
+                "VALUES (%(title)s, %(description)s, %(senRate)s, %(readRate)s, %(url)s, %(category)s, %(readby)s)")
+
+    demo_data=one_website_return(url)
+    cursor.execute(add_demo,demo_data)
+    
+    
+    cnx.commit()
+    cursor.close()
+    cnx.close()
+    
+    
+def multi_add(day="Fri"):
+    import mysql.connector
+    import json
+    from merge import total_website_return
+    
+    cnx = mysql.connector.connect(user='root', password='1111',
+                                  database='start')
+
+    cursor = cnx.cursor()
+
+    
+    add_demo = ("INSERT INTO demo "
+                "(title, description, senRate, readRate, url, category, readby) "
+                "VALUES (%(title)s, %(description)s, %(senRate)s, %(readRate)s, %(url)s, %(category)s, %(readby)s)")
+
+    demo_data=total_website_return(day)
+
+    for i in range(len(demo_data)):
+        cursor.execute(add_demo,demo_data[i])
+        
+    cnx.commit()
+    cursor.close()
+    cnx.close()
+    
+    
+    
+def deletefrom(url):
+    import mysql.connector
+    import json
+
+    cnx = mysql.connector.connect(user='root', password='1111',
+                                  database='start')
+
+    cursor = cnx.cursor()
+
+    delete_demo = ("DELETE FROM demo WHERE url = '"+url+"'")
+
+    cursor.execute(delete_demo)
+    
+    
+    cnx.commit()
+    cursor.close()
+    cnx.close()
+    
+def updatein(url):
+    import mysql.connector
+    import json
+
+    cnx = mysql.connector.connect(user='root', password='1111',
+                                  database='start')
+
+    cursor = cnx.cursor()
+
+    update_demo = ("UPDATE demo SET readby='read' WHERE url = '"+url+"'")
+
+    cursor.execute(update_demo)
+    
+    
+    cnx.commit()
+    cursor.close()
+    cnx.close()
+    
+single_add("http://www.bbc.com/news/world-europe-34602621")
---
+++ crawler/sqlcreate.py
@@ -0,0 +1,57 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+
+
+DB_NAME = 'start'
+
+TABLES = {}
+TABLES['demo'] = (
+    "CREATE TABLE `demo` ("
+    "  `title` varchar(1000) NOT NULL,"
+    "  `description` varchar(10000) NOT NULL,"
+    "  `senRate` float NOT NULL,"
+    "  `readRate` float NOT NULL,"
+    "  `url` varchar(1000) NOT NULL,"
+    "  `category` varchar(100) NOT NULL,"
+    "  `readby` varchar(100) NOT NULL,"
+    "  PRIMARY KEY (`url`)"
+    ") ENGINE=InnoDB")
+
+
+cnx = mysql.connector.connect(user='root',password='1111')
+cursor = cnx.cursor()
+
+
+def create_database(cursor):
+    try:
+        cursor.execute(
+            "CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(DB_NAME))
+    except mysql.connector.Error as err:
+        print("Failed creating database: {}".format(err))
+        exit(1)
+
+try:
+    cnx.database = DB_NAME    
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_BAD_DB_ERROR:
+        create_database(cursor)
+        cnx.database = DB_NAME
+    else:
+        print(err)
+        exit(1)
+        
+for name, ddl in TABLES.iteritems():
+    try:
+        print("Creating table {}: ".format(name))
+        cursor.execute(ddl)
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+            print("already exists.")
+        else:
+            print(err.msg)
+    else:
+        print("OK")
+
+cursor.close()
+cnx.close()
