5e2c6b4ff9d514a911b0794d07b535c4b8981b7e
Wenchao Yang <wenchao.yang10@hotmail.com>
2015-12-04 03:18:39 -0600

    Be ready to be called

--- src/merge.py
+++ src/merge.py
@@ -40,62 +40,61 @@ def one_website_print(url):
 
 
 if __name__ == '__main__':
-    # import sys
-    # if sys.argv[1] == '-u':
-    #     one_website_print(sys.argv[2])
-    # elif sys.argv[1] == '-d':
-    #     total_website_print(sys.argv[2])
-    # else:
-    #     print('merge.py -u <url>')
-    #     print('merge.py -d <day>')
 
     Cat_train = NBtrain('category')
-    # result = NBtest(text, train)
-    # print(result.prob)
-    # print(result.final_class)
     Senti_train = NBtrain('sentiment')
+
+    import sys
+    if sys.argv[1] == '-u':
+        one_website_print(sys.argv[2])
+    else:
+        print('merge.py -u <url>')
+
+
+    # result = NBtest(text, train)
+    # print(result.prob)
+    # print(result.final_class)
+
     # result = NBtest(text, train)
     # print(result.prob)
     # print(result.final_class)
 
 
-    start_time = time.time()
-    one_website_print("http://www.bbc.com/news/world-europe-34595409")
-    elapsed_time = time.time() - start_time
-    print(elapsed_time)
-
-
-
-    # print(total_website_return()[0])
-    # total_website_print()
-    start_time = time.time()
-    one_website_print("http://www.bbc.com/news/world-europe-34602621")
-    elapsed_time = time.time() - start_time
-    print(elapsed_time)
-
-    start_time = time.time()
-    one_website_print("http://blog.hubspot.com/blog/tabid/6307/bid/34010/How-to-Use-Photoshop-The-Ultimate-Guide-for-the-Design-Impaired-Marketer.aspx")
-    elapsed_time = time.time() - start_time
-    print(elapsed_time)
-
-    start_time = time.time()
-    one_website_print("http://america.aljazeera.com/articles/2015/12/3/san-bernardino-shooting-motive-searched.html")
-    elapsed_time = time.time() - start_time
-    print(elapsed_time)
-
-    start_time = time.time()
-    one_website_print("http://america.aljazeera.com/articles/2015/12/3/long-troubled-san-bernardino-in-shock-over-mass-shooting.html")
-    elapsed_time = time.time() - start_time
-    print(elapsed_time)
-
-    start_time = time.time()
-    one_website_print("http://america.aljazeera.com/articles/2015/12/3/mass-shooting-by-week.html")
-    elapsed_time = time.time() - start_time
-    print(elapsed_time)
-
-    start_time = time.time()
-    one_website_print("http://www.aljazeera.com/news/2015/10/afghan-refugee-shot-dead-enter-bulgaria-151016072352279.html")
-    elapsed_time = time.time() - start_time
-    print(elapsed_time)
+    # start_time = time.time()
+    # one_website_print("http://www.bbc.com/news/world-europe-34595409")
+    # elapsed_time = time.time() - start_time
+    # print(elapsed_time)
+    #
+    # # print(total_website_return()[0])
+    # # total_website_print()
+    # start_time = time.time()
+    # one_website_print("http://www.bbc.com/news/world-europe-34602621")
+    # elapsed_time = time.time() - start_time
+    # print(elapsed_time)
+    #
+    # start_time = time.time()
+    # one_website_print("http://blog.hubspot.com/blog/tabid/6307/bid/34010/How-to-Use-Photoshop-The-Ultimate-Guide-for-the-Design-Impaired-Marketer.aspx")
+    # elapsed_time = time.time() - start_time
+    # print(elapsed_time)
+    #
+    # start_time = time.time()
+    # one_website_print("http://america.aljazeera.com/articles/2015/12/3/san-bernardino-shooting-motive-searched.html")
+    # elapsed_time = time.time() - start_time
+    # print(elapsed_time)
+    #
+    # start_time = time.time()
+    # one_website_print("http://america.aljazeera.com/articles/2015/12/3/long-troubled-san-bernardino-in-shock-over-mass-shooting.html")
+    # elapsed_time = time.time() - start_time
+    # print(elapsed_time)
+    #
+    # start_time = time.time()
+    # one_website_print("http://america.aljazeera.com/articles/2015/12/3/mass-shooting-by-week.html")
+    # elapsed_time = time.time() - start_time
+    # print(elapsed_time)
+    #
+    # start_time = time.time()
+    # one_website_print("http://www.aljazeera.com/news/2015/10/afghan-refugee-shot-dead-enter-bulgaria-151016072352279.html")
+    # elapsed_time = time.time() - start_time
+    # print(elapsed_time)
 
 
